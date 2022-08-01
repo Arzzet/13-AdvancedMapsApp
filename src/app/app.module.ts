57c8c8994b8d747d2a75b80f15741922c9b3bcdf
Iwo <iwoskater3@hotmail.com>
2022-08-01 11:09:08 +0200

    New improved mapping project. Initial settings and geolocalization service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapsModule } from './maps/maps.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    MapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+if(!navigator.geolocation){
+  alert('La Geolocalización no está soportada en este navegador');
+  console.log('La Geolocalización no está soportada en este navegador');
+  
+}
+
+
 if (environment.production) {
   enableProdMode();
 }
